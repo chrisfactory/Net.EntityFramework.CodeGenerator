001571c5ad8db39143ee46e3b1e3fb158daaed31
chrisfactory <garphitech@gmail.com>
2023-05-30 09:09:07 +0200

    sp name provider

--- src/Net.EntityFramework.CodeGenerator.Core/Builder/Services/IntentDispatcher/IIntentDispatcher.cs
+++ src/Net.EntityFramework.CodeGenerator.Core/Builder/Services/IntentDispatcher/IIntentDispatcher.cs
@@ -2,5 +2,6 @@
 {
     public interface IIntentDispatcher
     {
+        Task DispatchAsync();
     }
 }
--- src/Net.EntityFramework.CodeGenerator.SqlServer/Entity/StoredProcedure/IStoredProcedurePackageBuilderExtensions.cs
+++ src/Net.EntityFramework.CodeGenerator.SqlServer/Entity/StoredProcedure/IStoredProcedurePackageBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Net.EntityFramework.CodeGenerator;
+using Net.EntityFramework.CodeGenerator.Core;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -35,10 +36,17 @@ namespace Microsoft.Extensions.DependencyInjection
         }
     }
 
-
+    public class SPInfos
+    {
+        public string TableName { get; set; }
+        public string TableFullName { get; set; }
+        public IEntityTypeTable EntityTable { get; set; }
+        public IReadOnlyCollection<IEntityColumn> ProjectionColumns { get; set; }
+        public IReadOnlyCollection<IEntityColumn> PrimaryKeys { get; set; }
+    }
     public interface IStoredProcedureNameProvider
     {
-        string Get();
+        string Get(SPInfos infos);
     }
     internal class FixedStoredProcedureNameProvider : IStoredProcedureNameProvider
     {
@@ -47,7 +55,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             this._name = name;
         }
-        public string Get()
+        public string Get(SPInfos infos)
         {
             return _name;
         }
@@ -56,19 +64,17 @@ namespace Microsoft.Extensions.DependencyInjection
 
     internal class SelectTableProcedureNameProvider : IStoredProcedureNameProvider
     {
-        public SelectTableProcedureNameProvider()
+
+        public string Get(SPInfos infos)
         {
+            var tableName = infos.TableName;
+            var keys = string.Empty;
+            foreach (var key in infos.PrimaryKeys)
+                keys += key.ColumnName;
+
+            return $"Select{tableName}By{keys}";
         }
 
-        public string Get()
-        {
-            return $"SelectBy{GetKeyName()}";
-        }
-
-        private string GetKeyName()
-        {
-            return "Truc";
-        }
     }
 
 
